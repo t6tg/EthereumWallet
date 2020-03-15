2999aa9cf3a4df869ba76a6c96547e11dbe7dcfa
thanawatgulati <thanawatgulatijames@gmail.com>
2020-03-15 13:28:59 +0700

    🧧 Create Wallet

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Nethereum.HdWallet;
+
+namespace EthereumWallet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DotNetEnv.Env.Load("./.env");
+            // var wallet = new Wallet(NBitcoin.Wordlist.English, NBitcoin.WordCount.TwentyFour);
+            var wallet = new Wallet(System.Environment.GetEnvironmentVariable("WALLET_SECRET"), "");
+            var account = wallet.GetAccount(0);
+            var memo = string.Join(" ", wallet.Words);
+            Console.WriteLine(account.Address);
+        }
+
+        //check Balance
+        public static void CheckBalance(string address)
+        {
+
+        }
+    }
+}

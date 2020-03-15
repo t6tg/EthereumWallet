d434e2f3df4636b3f1885ba0a64dc6f3eb112212
thanawatgulati <thanawatgulatijames@gmail.com>
2020-03-15 14:44:15 +0700

    🚚 Transfer

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Nethereum.HdWallet;
 using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
 
 namespace EthereumWallet
 {
@@ -14,9 +15,15 @@ namespace EthereumWallet
             // var wallet = new Wallet(NBitcoin.Wordlist.English, NBitcoin.WordCount.TwentyFour);
             var wallet = new Wallet(System.Environment.GetEnvironmentVariable("WALLET_SECRET"), "");
             var account = wallet.GetAccount(0);
-            var memo = string.Join(" ", wallet.Words);
-            Console.WriteLine(account.Address);
+            var receiverAccount = wallet.GetAccount(1);
+            // var memo = string.Join(" ", wallet.Words);
+            // Console.WriteLine(receiverAccount.Address);
+            // await CheckBalance(receiverAccount.Address);
+            await Transfer(account, receiverAccount.Address, 1);
+            Console.WriteLine($"MainAddress : {account.Address}");
             await CheckBalance(account.Address);
+            Console.WriteLine($"ReceiverAddress : {receiverAccount.Address}");
+            await CheckBalance(receiverAccount.Address);
         }
 
         //check Balance
@@ -26,6 +33,14 @@ namespace EthereumWallet
             var ethBalance = await client.Eth.GetBalance.SendRequestAsync(address);
             var balance = Web3.Convert.FromWei(ethBalance.Value);
             Console.WriteLine($"Balance : {balance}");
+        }
+
+        //Transfer
+        public static async Task Transfer(Account fromAccount, string receiverAddress, decimal amount)
+        {
+            var client = new Web3(fromAccount, SERVER);
+            var transaction = await client.Eth.GetEtherTransferService().TransferEtherAndWaitForReceiptAsync(receiverAddress, amount);
+            Console.WriteLine(transaction.TransactionHash);
         }
     }
 }
